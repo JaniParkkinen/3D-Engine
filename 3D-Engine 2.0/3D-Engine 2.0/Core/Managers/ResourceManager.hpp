4bd03d0c96369f963b1f1381eaccc18d1af468f3
Parkkinen Jani TTV14SP <janiparkkinen@kamk.fi>
2016-06-15 13:51:18 +0300

    ResourceManager in progress...

--- 3D-Engine 2.0/3D-Engine 2.0/Core/Managers/ResourceManager.hpp
+++ 3D-Engine 2.0/3D-Engine 2.0/Core/Managers/ResourceManager.hpp
@@ -1,7 +1,7 @@
 #ifndef Resourcemanager_h
 #define Resourcemanager_h
 #include <Core/Resource.h>
-
+#include <map>
 #include <iostream>
 #include <vector>
 //#include "Resource.h"
@@ -13,45 +13,35 @@ namespace Engine {
 	public:
 		ResourceManager() : currentScope(0), resourceCount(0) {};
 		virtual ~ResourceManager();
+		Resource* FindResourceByID(unsigned ID);
+		//void Clear();
+		void SetCurrentScope(unsigned scope);
+		const unsigned GetResourceCount() const { return resourceCount; }
 
+		static ResourceManager* GetInstance() {return instance;};
+		ResourceManager* LoadResource(std::string filepath);
+		void unLoadResource(int ID);
+		int GetResourceUsers(int ID);
+		//irrklang::ISoundEngine* soundEngine = irrklang::createIrrKlangDevice();
+
+	protected:
+		unsigned currentScope;
+		unsigned resourceCount;
+		std::map<unsigned, std::list<Resource*>> _resources;
+	
+	private:
+		ResourceManager(ResourceManager const&);
+		void operator=(ResourceManager const&);
+
+		std::vector<Resource*> _resources;
+		std::vector<Resource*>::iterator _it;
+		int ID_generator = 0;
+		static ResourceManager* instance;
+		//irrklang::ISoundSource* source;
+
+		Resource* LoadResourceType(std::string filepath);
+	
 	};
 }
-
-
-
-
-//class ResourceManager
-//{
-//public:
-//
-//	static ResourceManager* GetInstance();
-//	ResourceManager* LoadResource(std::string filepath);
-//	//Resource* LoadResource(std::string filepath);
-//	void UnLoadResource(int ID);
-//	int GetResourceUsers(int ID);
-//	//irrklang::ISoundEngine* soundEngine = irrklang::createIrrKlangDevice();
-//
-//private:
-//
-//	ResourceManager() {};
-//	~ResourceManager() {};
-//
-//	ResourceManager(ResourceManager const&);
-//	void operator=(ResourceManager const&);
-//
-//	//std::vector<Resource*> _resources;
-//	//std::vector<Resource*>::iterator _it;
-//	int ID_generator = 0;
-//	static ResourceManager* Instance;
-//	//irrklang::ISoundSource* source;
-//
-//	//Resource* LoadTextResource(std::string filepath);
-//	//Resource* LoadImageResource(std::string filepath);
-//	//Resource* LoadAudioResource(std::string filepath);
-//	//Resource* LoadFontResource(std::string filepath);
-//	//Resource* LoadObjectResource(std::string filepath);
-//	//void dropSoundEngine() { soundEngine->drop(); }; //ft snoop dogg
-//
-//};
 
 #endif
