e719d50255b28a4560e9bc287ea7fdceadb9bcd9
Parkkinen Jani TTV14SP <janiparkkinen@kamk.fi>
2016-06-15 10:05:07 +0300

    ResourceManager & Resource started

--- 3D-Engine 2.0/3D-Engine 2.0/Core/Managers/ResourceManager.hpp
+++ 3D-Engine 2.0/3D-Engine 2.0/Core/Managers/ResourceManager.hpp
@@ -1,43 +1,57 @@
-#ifndef RESOURCEMANAGER_H
-#define RESOURCEMANAGER_H
-
+#ifndef Resourcemanager_h
+#define Resourcemanager_h
+#include <Core/Resource.h>
 
 #include <iostream>
 #include <vector>
-#include "Resource.h"
-#include "..\..\Dependencies\include\irrKlang\irrKlang.h"
+//#include "Resource.h"
+//#include "..\..\Dependencies\include\irrKlang\irrKlang.h"
 
-class ResourceManager
-{
-public:
+namespace Engine {
+	class ResourceManager
+	{
+	public:
+		ResourceManager() : currentScope(0), resourceCount(0) {};
+		virtual ~ResourceManager();
 
-	static ResourceManager* GetInstance();
-	Resource* LoadResource(std::string filepath);
-	void UnLoadResource(int ID);
-	int GetResourceUsers(int ID);
-	irrklang::ISoundEngine* soundEngine = irrklang::createIrrKlangDevice();
+	};
+}
 
-private:
 
-	ResourceManager() {};
-	~ResourceManager() {};
 
-	ResourceManager(ResourceManager const&);
-	void operator=(ResourceManager const&);
 
-	std::vector<Resource*> _resources;
-	std::vector<Resource*>::iterator _it;
-	int ID_generator = 0;
-	static ResourceManager* Instance;
-	irrklang::ISoundSource* source;
-
-	Resource* LoadTextResource(std::string filepath);
-	Resource* LoadImageResource(std::string filepath);
-	Resource* LoadAudioResource(std::string filepath);
-	Resource* LoadFontResource(std::string filepath);
-	Resource* LoadObjectResource(std::string filepath);
-	void dropSoundEngine() { soundEngine->drop(); }; //ft snoop dogg
-
-};
+//class ResourceManager
+//{
+//public:
+//
+//	static ResourceManager* GetInstance();
+//	ResourceManager* LoadResource(std::string filepath);
+//	//Resource* LoadResource(std::string filepath);
+//	void UnLoadResource(int ID);
+//	int GetResourceUsers(int ID);
+//	//irrklang::ISoundEngine* soundEngine = irrklang::createIrrKlangDevice();
+//
+//private:
+//
+//	ResourceManager() {};
+//	~ResourceManager() {};
+//
+//	ResourceManager(ResourceManager const&);
+//	void operator=(ResourceManager const&);
+//
+//	//std::vector<Resource*> _resources;
+//	//std::vector<Resource*>::iterator _it;
+//	int ID_generator = 0;
+//	static ResourceManager* Instance;
+//	//irrklang::ISoundSource* source;
+//
+//	//Resource* LoadTextResource(std::string filepath);
+//	//Resource* LoadImageResource(std::string filepath);
+//	//Resource* LoadAudioResource(std::string filepath);
+//	//Resource* LoadFontResource(std::string filepath);
+//	//Resource* LoadObjectResource(std::string filepath);
+//	//void dropSoundEngine() { soundEngine->drop(); }; //ft snoop dogg
+//
+//};
 
 #endif
---
+++ 3D-Engine 2.0/3D-Engine 2.0/Core/Resource.h
@@ -0,0 +1,48 @@
+#ifndef Resource_H
+#define Resource_H
+#include <string>
+
+//http://gamedev.stackexchange.com/questions/17066/designing-a-resourcemanager-class/17082#17082
+namespace Engine {
+	typedef enum class Resource_Type
+	{
+		Resource_Null = 0,
+		Resource_Graphics = 1,
+		Resource_Movie = 2,
+		Resource_Audio = 3,
+		Resource_Text = 4,
+	}Resource_Type;
+
+	class Resource //: public EngineObject
+	{
+	public:
+		Resource() : resourceID(0), _scope(0), _type(Resource_Type::Resource_Null) {}
+		~Resource() {}
+		void Load();
+		void Unload();
+
+		void SetResourceID(unsigned ID) { resourceID = ID; }
+		unsigned GetResourceID() const { return resourceID; }
+
+		void SetFilename(std::string filename) { _filename = filename; }
+		std::string GetFilename() const { return _filename; }
+
+		void SetResourceType(Resource_Type type) { _type = type; }
+		Resource_Type GetResourceType() const { return _type; }
+
+		void SetResourceScope(unsigned scope) { _scope = scope; }
+		signed GetResourceScope() const { return _scope; }
+
+		bool IsLoaded() const { return _loaded; }
+		void SetLoaded(bool value) { _loaded = value; }
+
+	protected:
+		signed resourceID;
+		signed _scope;
+		std::string _filename;
+		Resource_Type _type;
+		bool _loaded;
+	};
+}
+
+#endif // !Resource_H
