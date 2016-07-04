32c7d16c53a65defb82263167495cbc40f0e5b64
Parkkinen Jani TTV14SP <janiparkkinen@kamk.fi>
2016-07-04 13:44:03 +0300

    FBX loading & animation [WIP]

--- 3D-Engine 2.0/3D-Engine 2.0/Core/Components/Mesh.cpp
+++ 3D-Engine 2.0/3D-Engine 2.0/Core/Components/Mesh.cpp
@@ -1,5 +1,21 @@
 #include <Core/Components/Mesh.h>
 
+
+
+void Engine::Mesh::VertexBoneData::AddBoneData(unsigned BoneID, float Weight)
+{
+	for (unsigned i = 0; i < ARRAY_SIZE_IN_ELEMENTS(IDs); i++)
+	{
+		if (Weights[i] == 0.0)
+		{
+			IDs[i] = BoneID;
+			Weights[i] = Weight;
+			return;
+		}
+	}
+	assert(0);
+}
+
 Engine::Mesh::Mesh()
 {
 
@@ -10,39 +26,29 @@ Engine::Mesh::~Mesh()
 
 }
 
-//bool Engine::Mesh::LoadMesh(const std::string& Filename)
-//{
-//	Clear();
-//
-//	Ret = false;
-//	Assimp::Importer Importer;
-//
-//	const aiScene* pScene = Importer.ReadFile(Filename.c_str(), aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_FlipUVs);
-//
-//	if (pScene)
-//	{
-//		Ret = InitFromScene(pScene, Filename);
-//	}
-//	else
-//	{
-//		printf("Error parsing '%s': '%s'\n", Filename.c_str(), Importer.GetErrorString());
-//	}
-//
-//	return Ret;
-//}
-
 bool Engine::Mesh::InitFromScene(const aiScene* pScene, const std::string& Filename)
 {
+	std::vector<VertexBoneData> Bones;
+	unsigned NumVertices = 0;
+
 	m_Entries.resize(pScene->mNumMeshes);
-	m_Textures.resize(pScene->mNumMaterials);
+
+	//m_Textures.resize(pScene->mNumMaterials);
 
 	for (unsigned i = 0; i < m_Entries.size(); i++)
 	{
 		const aiMesh* paiMesh = pScene->mMeshes[i];
 		InitMesh(i, paiMesh);
+
+		NumVertices += pScene->mMeshes[i]->mNumVertices;
 	}
 
+	Bones.resize(NumVertices);
+
+	//TODO opengl code
+
 	//return InitMaterials(pScene, Filename);
+	return 0;
 }
 
 void Engine::Mesh::InitMesh(unsigned Index, const aiMesh* paiMesh)
@@ -161,7 +167,180 @@ void Engine::Mesh::InitMesh(unsigned Index, const aiMesh* paiMesh)
 //	glDisableVertexAttribArray(2);
 //}
 
+void Engine::Mesh::LoadBones(unsigned MeshIndex, const aiMesh* pMesh, std::vector<VertexBoneData>& Bones)
+{
+	for (unsigned i = 0; i < pMesh->mNumBones; i++)
+	{
+		unsigned BoneIndex = 0;
+		std::string BoneName(pMesh->mBones[i]->mName.data);
 
+		if (m_BoneMapping.find(BoneName) == m_BoneMapping.end())
+		{
+			BoneIndex = m_NumBones;
+			m_NumBones++;
+			BoneInfo bi;
+			m_BoneInfo.push_back(bi);
+		}
+		else
+		{
+			BoneIndex = m_BoneMapping[BoneName];
+		}
 
+		m_BoneMapping[BoneName] = BoneIndex;
+		m_BoneInfo[BoneIndex].BoneOffset = pMesh->mBones[i]->mOffsetMatrix;
 
+		for (unsigned j = 0; j < pMesh->mBones[i]->mNumWeights; j++)
+		{
+			unsigned VertexID = m_Entries[MeshIndex].BaseVertex + pMesh->mBones[i]->mWeights[j].mVertexId;
+			float Weight = pMesh->mBones[i]->mWeights[j].mWeight;
+			Bones[VertexID].AddBoneData(BoneIndex, Weight);
+		}
+	}
+}
 
+glm::mat4 Engine::Mesh::BoneTransform(float TimeInSeconds, std::vector<aiMatrix4x4>& Transforms)
+{
+	glm::mat4 Identity;
+	Identity.InitIdentity();
+
+	float TicksPerSecond = (float)(m_pScene->mAnimations[0]->mTicksPerSecond != 0 ? m_pScene->mAnimations[0]->mTicksPerSecond : 25.0f);
+	float TimeInTicks = TimeInSeconds *TicksPerSecond;
+	float AnimationTime = fmod(TimeInTicks, m_pScene->mAnimations[0]->mDuration);
+
+	ReadNodeHeirarchy(AnimationTime, m_pScene->mRootNode, Identity);
+
+	Transforms.resize(m_NumBones);
+
+	for (unsigned i = 0; i < m_NumBones; i++)
+	{
+		Transforms[i] = m_BoneInfo[i].FinalTransformation;
+	}
+}
+
+void Engine::Mesh::ReadNodeHeirarchy(float AnimationTime, const aiNode* pNode, const aiMatrix4x4 ParentTransform)
+{
+	std::string NodeName(pNode->mName.data);
+
+	const aiAnimation* pAnimation = m_pScene->mAnimations[0];
+	
+	aiMatrix4x4 NodeTransformation(pNode->mTransformation);
+	
+	const aiNodeAnim* pNodeAnim = FindNodeAnim(pAnimation, NodeName);
+
+	if (pNodeAnim)
+	{
+		aiVector3D Scaling;
+		CalcInterpolatedScaling(Scaling, AnimationTime, pNodeAnim);
+		glm::mat4 ScalingM;
+		ScalingM.InitScaleTransform(Scaling.x, Scaling.y, Scaling.z);
+
+		aiQuaternion RotationQ;
+		CalcInterpolatedRotation(RotationQ, AnimationTime, pNodeAnim);
+		glm::mat4 RotationM = glm::mat4(RotationQ.GetMatrix());
+
+		aiVector3D Translation;
+		CalcInterpolatedPosition(Translation, AnimationTime, pNodeAnim);
+		glm::mat4 TranslationM;
+		TranslationM.InitTranslationTransform(Translation.x, Translation.y, Translation.z);
+
+		NodeTransformation = TranslationM * RotationM * ScalingM;
+	}
+
+	aiMatrix4x4 GlobalTransformation = ParentTransform * NodeTransformation;
+
+	if (m_BoneMapping.find(NodeName) != m_BoneMapping.end())
+	{
+		unsigned BoneIndex = m_BoneMapping[NodeName];
+		m_BoneInfo[BoneIndex].FinalTransformation = m_GlobalInverseTransform * GlobalTransformation * m_BoneInfo[BoneIndex].BoneOffset;
+	}
+
+	for (unsigned i = 0; i < pNode->mNumChildren; i++)
+	{
+		ReadNodeHeirarchy(AnimationTime, pNode->mChildren[i], GlobalTransformation);
+	}
+}
+
+unsigned Engine::Mesh::FindRotation(float AnimationTime, const aiNodeAnim* pNodeAnim)
+{
+	assert(pNodeAnim->mNumRotationKeys > 0);
+
+	for (unsigned i = 0; i < pNodeAnim->mNumRotationKeys - 1; i++)
+	{
+		if (AnimationTime < (float)pNodeAnim->mRotationKeys[i + 1].mTime)
+		{
+			return i;
+		}
+	}
+
+	assert(0);
+}
+
+void Engine::Mesh::CalcInterpolatedRotation(aiQuaternion& Out, float AnimationTime, const aiNodeAnim* pNodeAnim)
+{
+	if (pNodeAnim->mNumRotationKeys == 1)
+	{
+		Out = pNodeAnim->mRotationKeys[0].mValue;
+		return;
+	}
+
+	unsigned RotationIndex = FindRotation(AnimationTime, pNodeAnim);
+	unsigned NextRotationIndex = (RotationIndex + 1);
+	assert(NextRotationIndex < pNodeAnim->mNumRotationKeys);
+	float DeltaTime = pNodeAnim->mRotationKeys[NextRotationIndex].mTime - pNodeAnim->mRotationKeys[RotationIndex].mTime;
+	float Factor = (AnimationTime - (float)pNodeAnim->mRotationKeys[RotationIndex].mTime) / DeltaTime;
+	assert(Factor >= 0.0f && Factor <= 1.0f);
+	const aiQuaternion& StartRotationQ = pNodeAnim->mRotationKeys[RotationIndex].mValue;
+	const aiQuaternion& EndRotationQ = pNodeAnim->mRotationKeys[NextRotationIndex].mValue;
+	aiQuaternion::Interpolate(Out, StartRotationQ, EndRotationQ, Factor);
+	Out = Out.Normalize();
+}
+
+unsigned Engine::Mesh::FindScaling(float AnimationTime, const aiNodeAnim* pNodeAnim)
+{
+	assert(pNodeAnim->mNumScalingKeys > 0);
+
+	for (unsigned i = 0; i < pNodeAnim->mNumScalingKeys - 1; i++)
+	{
+		if (AnimationTime < (float)pNodeAnim->mScalingKeys[i + 1].mTime)
+		{
+			return i;
+		}
+	}
+	assert(0);
+
+	return 0;
+}
+
+void Engine::Mesh::CalcInterpolatedScaling(aiVector3D Out, float AnimationTime, const aiNodeAnim* pNodeAnim)
+{
+	if (pNodeAnim->mNumScalingKeys == 1)
+	{
+		Out = pNodeAnim->mScalingKeys[0].mValue;
+		return;
+	}
+	//write findscaling!!!!
+	unsigned ScalingIndex = FindScaling(AnimationTime, pNodeAnim);
+	unsigned NextScalingIndex = (ScalingIndex + 1);
+	assert(NextScalingIndex < pNodeAnim->mNumScalingKeys);
+	float DeltaTime = (float)(pNodeAnim->mScalingKeys[NextScalingIndex].mTime - pNodeAnim->mScalingKeys[ScalingIndex].mTime);
+	float Factor = (AnimationTime - (float)pNodeAnim->mScalingKeys[ScalingIndex].mTime) / DeltaTime;
+	assert(Factor >= 0.0f && Factor <= 1.0f);
+	const aiVector3D& Start = pNodeAnim->mScalingKeys[ScalingIndex].mValue;
+	const aiVector3D& End = pNodeAnim->mScalingKeys[NextScalingIndex].mValue;
+	aiVector3D Delta = End - Start;
+	Out = Start + Factor * Delta;
+}
+
+const aiNodeAnim* Engine::Mesh::FindNodeAnim(const aiAnimation* pAnimation, const std::string NodeName)
+{
+	for (unsigned i = 0; i < pAnimation->mNumChannels; i++)
+	{
+		const aiNodeAnim* pNodeAnim = pAnimation->mChannels[i];
+
+		if (std::string(pNodeAnim->mNodeName.data) == NodeName)
+		{
+			return pNodeAnim;
+		}
+	}
+	return NULL;
+}
--- 3D-Engine 2.0/3D-Engine 2.0/Core/Components/Mesh.h
+++ 3D-Engine 2.0/3D-Engine 2.0/Core/Components/Mesh.h
@@ -8,6 +8,8 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <map>
+
 
 #include <Core/Components/Texture.hpp>
 
@@ -31,23 +33,49 @@ struct Vertex
 		m_tangent = tangent;
 	}
 };
+#define NUM_BONES_PER_VEREX 4
+
+struct BoneInfo
+{
+	aiMatrix4x4 BoneOffset;
+	aiMatrix4x4 mOffsetMatrix;
+	aiMatrix4x4 FinalTransformation;
+};
+
+struct VertexBoneData
+{
+	unsigned IDs[NUM_BONES_PER_VEREX];
+	float Weights[NUM_BONES_PER_VEREX];
+
+	void AddBoneData(unsigned BoneID, float Weight);
+};
+
 namespace Engine
 {
-	class Mesh
+	class Mesh : public VertexBoneData
 	{
 	public:
 		Mesh();
 
 		~Mesh();
 
-		bool LoadMesh(const std::string& Filename);
+		bool LoadMesh(const std::string& Filename) { return 1; };
 
-		void Render();
+		void Render() {};
 
 		bool InitFromScene(const aiScene* pScene, const std::string& Filename);
 		void InitMesh(unsigned int Index, const aiMesh* paiMesh);
 		//bool InitMaterials(const aiScene* pScene, const std::string& Filename);
 		//void Clear();
+		void LoadBones(unsigned MeshIndex, const aiMesh* pMesh, std::vector<VertexBoneData>& Bones);
+		void VertexBoneData::AddBoneData(unsigned BoneID, float Weight);
+		glm::mat4 BoneTransform(float TimeInSeconds, std::vector<aiMatrix4x4>& Transforms);
+		void ReadNodeHeirarchy(float AnimationTime, const aiNode* pNode, const aiMatrix4x4 ParentTransform);
+		void CalcInterpolatedRotation(aiQuaternion& Out, float AnimationTime, const aiNodeAnim* pNodeAnim);
+		void CalcInterpolatedScaling(aiVector3D Out, float AnimationTime, const aiNodeAnim* pNodeAnim);
+		unsigned FindScaling(float AnimationTime, const aiNodeAnim* pNodeAnim);
+		unsigned FindRotation(float AnimationTime, const aiNodeAnim* pNodeAnim);
+		const aiNodeAnim* FindNodeAnim(const aiAnimation* pAnimation, const std::string Nodename);
 
 	private:
 		//bool InitFromScene(const aiScene* pScene, const std::string& Filename);
@@ -56,21 +84,31 @@ namespace Engine
 		//void Clear();
 
 #define INVALID_MATERIAL 0xFFFFFFFF
+
 		struct MeshEntry {
-			MeshEntry();
-			~MeshEntry();
+			MeshEntry() {};
+			~MeshEntry() {};
 
 			bool Init(const std::vector<Vertex>& Vertices,
-				const std::vector<unsigned>& Indices);
+				const std::vector<unsigned>& Indices) {
+				return 1;
+			};
 
 			GLuint VB;
 			GLuint IB;
 			unsigned NumIndices;
 			unsigned MaterialIndex;
+			unsigned BaseVertex;
 		};
+		std::map<std::string, unsigned> m_BoneMapping;
+		std::vector<BoneInfo> m_BoneInfo;
 		std::vector<MeshEntry> m_Entries;
-		std::vector<Engine::Texture*> m_Textures;
+		unsigned m_NumBones;
+		//std::vector<Engine::Texture*> m_Textures;
 		bool Ret;
+		const aiScene* m_pScene;
+		glm::mat4 m_GlobalInverseTransform;
+		
 	};
 }
 #endif
